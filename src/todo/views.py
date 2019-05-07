a696ce5052dd4e4379a955ebd0e416c5d4b935a6
UsuallyChris <schoonc@gmail.com>
2019-05-06 20:19:36 -0700

    index.html renders todo objects from the database

--- src/todo/views.py
+++ src/todo/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Todo
 
 # Create your views here.
 def index(request):
-  context = {}
-  return render(request, 'todo/index.html')
+  todo_list = Todo.objects.order_by('id')
+  context = {'todo_list' : todo_list}
+  return render(request, 'todo/index.html', context)
